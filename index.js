b08ea694b8fb58f53c410037f63966063b3ffb43
Graham Fairweather <graham.fairweather@ispy.se>
2017-03-27 10:19:19 +0200

    Remove yar and jscs, cleanup

--- index.js
+++ index.js
@@ -22,41 +22,18 @@
  * alt="npm version" height="18">
  * </a>
  *
- * toInteger module.
+ * toInteger converts 'argument' to an integral numeric value.
  *
- * <h2>ECMAScript compatibility shims for legacy JavaScript engines</h2>
- * `es5-shim.js` monkey-patches a JavaScript context to contain all EcmaScript 5
- * methods that can be faithfully emulated with a legacy JavaScript engine.
+ * @see {@link http://www.ecma-international.org/ecma-262/6.0/#sec-tointeger|7.1.4 ToInteger ( argument )}
  *
- * `es5-sham.js` monkey-patches other ES5 methods as closely as possible.
- * For these methods, as closely as possible to ES5 is not very close.
- * Many of these shams are intended only to allow code to be written to ES5
- * without causing run-time errors in older engines. In many cases,
- * this means that these shams cause many ES5 methods to silently fail.
- * Decide carefully whether this is what you want. Note: es5-sham.js requires
- * es5-shim.js to be able to work properly.
- *
- * `json3.js` monkey-patches the EcmaScript 5 JSON implimentation faithfully.
- *
- * `es6.shim.js` provides compatibility shims so that legacy JavaScript engines
- * behave as closely as possible to ECMAScript 6 (Harmony).
- *
- * @version 1.1.1
+ * @version 1.1.2
  * @author Xotic750 <Xotic750@gmail.com>
  * @copyright  Xotic750
  * @license {@link <https://opensource.org/licenses/MIT> MIT}
  * @module to-integer-x
  */
 
-/* jslint maxlen:80, es6:true, white:true */
-
-/* jshint bitwise:true, camelcase:true, curly:true, eqeqeq:true, forin:true,
-   freeze:true, futurehostile:true, latedef:true, newcap:true, nocomma:true,
-   nonbsp:true, singleGroups:true, strict:true, undef:true, unused:true,
-   es3:true, esnext:true, plusplus:true, maxparams:1, maxdepth:1,
-   maxstatements:7, maxcomplexity:4 */
-
-/* eslint strict: 1, max-statements: 1 */
+/* eslint strict: 1 */
 
 /* global module */
 
@@ -64,19 +41,16 @@
 
   'use strict';
 
-  var $Number = Number;
-  var $isNaN = Number.isNaN;
-  var $isFinite = Number.isFinite;
-  var $sign = Math.sign;
-  var $floor = Math.floor;
-  var $abs = Math.abs;
-  var zero = 0;
+  var $isNaN = require('is-nan');
+  var $isFinite = require('is-finite-x');
+  var $sign = require('math-sign-x');
 
   /**
    * Converts `value` to an integer.
    *
    * @param {*} value The value to convert.
    * @return {number} Returns the converted integer.
+   *
    * @example
    * var toInteger = require('to-integer-x');
    * toInteger(3); // 3
@@ -85,13 +59,13 @@
    * toInteger('3'); // 3
    */
   module.exports = function toInteger(value) {
-    var number = $Number(value);
+    var number = Number(value);
     if ($isNaN(number)) {
-      return zero;
+      return 0;
     }
-    if (number === zero || !$isFinite(number)) {
+    if (number === 0 || !$isFinite(number)) {
       return number;
     }
-    return $sign(number) * $floor($abs(number));
+    return $sign(number) * Math.floor(Math.abs(number));
   };
 }());
