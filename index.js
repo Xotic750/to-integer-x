34c57670e613755c7634705504bc62dfda579401
Xotic750 <xotic750@gmail.com>
2017-07-02 15:39:44 +0200

    Update boilerplate and dependecies

--- index.js
+++ index.js
@@ -28,46 +28,41 @@
  *
  * @see {@link http://www.ecma-international.org/ecma-262/6.0/#sec-tointeger|7.1.4 ToInteger ( argument )}
  *
- * @version 1.2.0
+ * @version 1.3.0
  * @author Xotic750 <Xotic750@gmail.com>
  * @copyright  Xotic750
  * @license {@link <https://opensource.org/licenses/MIT> MIT}
  * @module to-integer-x
  */
 
-/* eslint strict: 1 */
+'use strict';
 
-/* global module */
+var $isNaN = require('is-nan');
+var $isFinite = require('is-finite-x');
+var $sign = require('math-sign-x');
 
-;(function () { // eslint-disable-line no-extra-semi
+/**
+ * Converts `value` to an integer.
+ *
+ * @param {*} value - The value to convert.
+ * @returns {number} Returns the converted integer.
+ *
+ * @example
+ * var toInteger = require('to-integer-x');
+ * toInteger(3); // 3
+ * toInteger(Number.MIN_VALUE); // 0
+ * toInteger(Infinity); // 1.7976931348623157e+308
+ * toInteger('3'); // 3
+ */
+module.exports = function ToInteger(value) {
+  var number = Number(value);
+  if ($isNaN(number)) {
+    return 0;
+  }
 
-  'use strict';
+  if (number === 0 || $isFinite(number) === false) {
+    return number;
+  }
 
-  var $isNaN = require('is-nan');
-  var $isFinite = require('is-finite-x');
-  var $sign = require('math-sign-x');
-
-  /**
-   * Converts `value` to an integer.
-   *
-   * @param {*} value The value to convert.
-   * @return {number} Returns the converted integer.
-   *
-   * @example
-   * var toInteger = require('to-integer-x');
-   * toInteger(3); // 3
-   * toInteger(Number.MIN_VALUE); // 0
-   * toInteger(Infinity); // 1.7976931348623157e+308
-   * toInteger('3'); // 3
-   */
-  module.exports = function ToInteger(value) {
-    var number = Number(value);
-    if ($isNaN(number)) {
-      return 0;
-    }
-    if (number === 0 || !$isFinite(number)) {
-      return number;
-    }
-    return $sign(number) * Math.floor(Math.abs(number));
-  };
-}());
+  return $sign(number) * Math.floor(Math.abs(number));
+};
--- tests/spec/test.js
+++ tests/spec/test.js
@@ -1,60 +1,48 @@
-/* jslint maxlen:80, es6:true, white:true */
+'use strict';
 
-/* jshint bitwise:true, camelcase:true, curly:true, eqeqeq:true, forin:true,
-   freeze:true, futurehostile:true, latedef:true, newcap:true, nocomma:true,
-   nonbsp:true, singleGroups:true, strict:true, undef:true, unused:true,
-   es3:true, esnext:true, plusplus:true, maxparams:1, maxdepth:2,
-   maxstatements:11, maxcomplexity:3 */
-
-/* eslint strict: 1, max-lines: 1, symbol-description: 1, max-nested-callbacks: 1,
-   max-statements: 1 */
-
-/* global JSON:true, expect, module, require, describe, it, returnExports */
-
-;(function () { // eslint-disable-line no-extra-semi
-
-  'use strict';
-
-  var toInteger;
-  if (typeof module === 'object' && module.exports) {
-    require('es5-shim');
-    require('es5-shim/es5-sham');
-    if (typeof JSON === 'undefined') {
-      JSON = {};
-    }
-    require('json3').runInContext(null, JSON);
-    require('es6-shim');
-    var es7 = require('es7-shim');
-    Object.keys(es7).forEach(function (key) {
-      var obj = es7[key];
-      if (typeof obj.shim === 'function') {
-        obj.shim();
-      }
-    });
-    toInteger = require('../../index.js');
-  } else {
-    toInteger = returnExports;
+var toInteger;
+if (typeof module === 'object' && module.exports) {
+  require('es5-shim');
+  require('es5-shim/es5-sham');
+  if (typeof JSON === 'undefined') {
+    JSON = {};
   }
-
-  describe('toInteger', function () {
-    it('Basic', function () {
-      expect(Object.is(0, toInteger(NaN))).toBe(true, 'NaN coerces to +0');
-      [0, Infinity, 42].forEach(function (num) {
-        expect(Object.is(num, toInteger(num))).toBe(true, num + ' returns itself');
-        expect(Object.is(-num, toInteger(-num))).toBe(true, '-' + num + ' returns itself');
-      });
-      expect(toInteger(Math.PI)).toBe(3, 'pi returns 3');
-      expect(function () {
-        var uncoercibleObject = {
-          toString: function () {
-            return {};
-          },
-          valueOf: function () {
-            return {};
-          }
-        };
-        return toInteger(uncoercibleObject);
-      }).toThrow();
-    });
+  require('json3').runInContext(null, JSON);
+  require('es6-shim');
+  var es7 = require('es7-shim');
+  Object.keys(es7).forEach(function (key) {
+    var obj = es7[key];
+    if (typeof obj.shim === 'function') {
+      obj.shim();
+    }
   });
-}());
+  toInteger = require('../../index.js');
+} else {
+  toInteger = returnExports;
+}
+
+describe('toInteger', function () {
+  it('Basic', function () {
+    expect(Object.is(0, toInteger(NaN))).toBe(true, 'NaN coerces to +0');
+    [
+      0,
+      Infinity,
+      42
+    ].forEach(function (num) {
+      expect(Object.is(num, toInteger(num))).toBe(true, num + ' returns itself');
+      expect(Object.is(-num, toInteger(-num))).toBe(true, '-' + num + ' returns itself');
+    });
+    expect(toInteger(Math.PI)).toBe(3, 'pi returns 3');
+    expect(function () {
+      var uncoercibleObject = {
+        toString: function () {
+          return {};
+        },
+        valueOf: function () {
+          return {};
+        }
+      };
+      return toInteger(uncoercibleObject);
+    }).toThrow();
+  });
+});
