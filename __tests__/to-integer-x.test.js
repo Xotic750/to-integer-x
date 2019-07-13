5ea2dd6339a32da8c86553fc54d45d14ce9bdf54
Graham Fairweather <xotic750@gmail.com>
2019-07-14 01:46:43 +0200

    Terraformed
    Automated build

--- __tests__/to-integer-x.test.js
+++ __tests__/to-integer-x.test.js
@@ -1,17 +1,19 @@
-'use strict';
+let lib;
 
-var lib;
 if (typeof module === 'object' && module.exports) {
   require('es5-shim');
   require('es5-shim/es5-sham');
+
   if (typeof JSON === 'undefined') {
     JSON = {};
   }
+
   require('json3').runInContext(null, JSON);
   require('es6-shim');
-  var es7 = require('es7-shim');
-  Object.keys(es7).forEach(function (key) {
-    var obj = es7[key];
+  const es7 = require('es7-shim');
+  Object.keys(es7).forEach(function(key) {
+    const obj = es7[key];
+
     if (typeof obj.shim === 'function') {
       obj.shim();
     }
@@ -21,71 +23,62 @@ if (typeof module === 'object' && module.exports) {
   lib = returnExports;
 }
 
-var toInteger = lib.toInteger;
-var toInteger2016 = lib.toInteger2016;
-var toInteger2018 = lib.toInteger2018;
+const {toInteger} = lib;
+const {toInteger2016} = lib;
+const {toInteger2018} = lib;
 
-describe('toInteger', function () {
-  describe('basic', function () {
-    it('toInteger is a function', function () {
+describe('toInteger', function() {
+  describe('basic', function() {
+    it('toInteger is a function', function() {
       expect(typeof toInteger2016).toBe('function');
     });
 
-    it('toInteger2016 is a function', function () {
+    it('toInteger2016 is a function', function() {
       expect(typeof toInteger2016).toBe('function');
     });
 
-    it('toInteger not to be toInteger2016', function () {
+    it('toInteger not to be toInteger2016', function() {
       expect(toInteger).not.toBe(toInteger2016);
     });
 
-    it('toInteger to be toInteger2018', function () {
+    it('toInteger to be toInteger2018', function() {
       expect(toInteger).toBe(toInteger2018);
     });
   });
 
-  describe('toInteger2016', function () {
-    it('Basic', function () {
+  describe('toInteger2016', function() {
+    it('basic', function() {
       expect(Object.is(0, toInteger2016(NaN))).toBe(true, 'NaN coerces to +0');
-      [
-        -0,
-        0,
-        Infinity,
-        42
-      ].forEach(function (num) {
-        expect(Object.is(num, toInteger2016(num))).toBe(true, num + ' returns itself');
-        expect(Object.is(-num, toInteger2016(-num))).toBe(true, '-' + num + ' returns itself');
+      [-0, 0, Infinity, 42].forEach(function(num) {
+        expect(Object.is(num, toInteger2016(num))).toBe(true, `${num} returns itself`);
+        expect(Object.is(-num, toInteger2016(-num))).toBe(true, `-${num} returns itself`);
       });
       expect(toInteger2016('0b10')).toBe(2, 'binary returns 2');
       expect(toInteger2016('0o10')).toBe(8, 'octal returns 8');
       expect(toInteger2016(' \t\r\n\u180e1 \t\r\n\u180e')).toBe(1, 'whitespaces are trimmed');
       expect(toInteger2016('\u0085\u200b\ufffe10\u0085\u200b\ufffe')).toBe(0, 'non-whitespaces are not trimmed');
       expect(toInteger2016(Math.PI)).toBe(3, 'pi returns 3');
-      expect(function () {
-        var uncoercibleObject = {
-          toString: function () {
+      expect(function() {
+        const uncoercibleObject = {
+          toString() {
             return {};
           },
-          valueOf: function () {
+          valueOf() {
             return {};
-          }
+          },
         };
+
         return toInteger2016(uncoercibleObject);
       }).toThrow();
     });
   });
 
-  describe('toInteger2018', function () {
-    it('Basic', function () {
+  describe('toInteger2018', function() {
+    it('basic', function() {
       expect(Object.is(0, toInteger2018(NaN))).toBe(true, 'NaN coerces to +0');
-      [
-        -0,
-        0,
-        Infinity,
-        42
-      ].forEach(function (num) {
-        expect(Object.is(num, toInteger2018(num))).toBe(true, num + ' returns itself');
-        expect(Object.is(-num, toInteger2018(-num))).toBe(true, '-' + num + ' returns itself');
+      [-0, 0, Infinity, 42].forEach(function(num) {
+        expect(Object.is(num, toInteger2018(num))).toBe(true, `${num} returns itself`);
+        expect(Object.is(-num, toInteger2018(-num))).toBe(true, `-${num} returns itself`);
       });
       expect(toInteger2018('0b10')).toBe(2, 'binary returns 2');
       expect(toInteger2018('0o10')).toBe(8, 'octal returns 8');
@@ -93,15 +86,16 @@ describe('toInteger', function () {
       expect(toInteger2018('\u0085\u200b\ufffe10\u0085\u200b\ufffe')).toBe(0, 'non-whitespaces are not trimmed');
       expect(toInteger2018('\u180e10\u180e')).toBe(0, 'non-whitespaces are not trimmed');
       expect(toInteger2018(Math.PI)).toBe(3, 'pi returns 3');
-      expect(function () {
-        var uncoercibleObject = {
-          toString: function () {
+      expect(function() {
+        const uncoercibleObject = {
+          toString() {
             return {};
           },
-          valueOf: function () {
+          valueOf() {
             return {};
-          }
+          },
         };
+
         return toInteger2018(uncoercibleObject);
       }).toThrow();
     });
--- src/to-integer-x.js
+++ src/to-integer-x.js
@@ -1,28 +1,29 @@
 /**
  * @file ToInteger converts 'argument' to an integral numeric value.
  * @see {@link http://www.ecma-international.org/ecma-262/6.0/#sec-tointeger|7.1.4 ToInteger ( argument )}
- * @version 3.0.0
- * @author Xotic750 <Xotic750@gmail.com>
- * @copyright  Xotic750
+ * @version 3.0.0.
+ * @author Xotic750 <Xotic750@gmail.com>.
+ * @copyright  Xotic750.
  * @license {@link <https://opensource.org/licenses/MIT> MIT}
- * @module to-integer-x
+ * @module To-integer-x.
  */
 
-'use strict';
+const libToNumber = require('to-number-x');
 
-var libToNumber = require('to-number-x');
-var toNumber2016 = libToNumber.toNumber2016;
-var toNumber2018 = libToNumber.toNumber2018;
-var numberIsNaN = require('is-nan-x');
-var numberIsFinite = require('is-finite-x');
-var libMathSign = require('math-sign-x');
-var mathSign2016 = libMathSign.sign2016;
-var mathSign2018 = libMathSign.sign2018;
-var mathFloor = Math.floor;
-var mathAbs = Math.abs;
+const {toNumber2016} = libToNumber;
+const {toNumber2018} = libToNumber;
+const numberIsNaN = require('is-nan-x');
+const numberIsFinite = require('is-finite-x');
+const libMathSign = require('math-sign-x');
 
-var $toInteger2016 = function toInteger2016(value) {
-  var number = toNumber2016(value);
+const mathSign2016 = libMathSign.sign2016;
+const mathSign2018 = libMathSign.sign2018;
+const mathFloor = Math.floor;
+const mathAbs = Math.abs;
+
+const $toInteger2016 = function toInteger2016(value) {
+  const number = toNumber2016(value);
+
   if (numberIsNaN(number)) {
     return 0;
   }
@@ -34,8 +35,9 @@ var $toInteger2016 = function toInteger2016(value) {
   return mathSign2016(number) * mathFloor(mathAbs(number));
 };
 
-var $toInteger2018 = function toInteger2018(value) {
-  var number = toNumber2018(value);
+const $toInteger2018 = function toInteger2018(value) {
+  const number = toNumber2018(value);
+
   if (numberIsNaN(number)) {
     return 0;
   }
@@ -54,7 +56,7 @@ module.exports = {
   toInteger: $toInteger2018,
 
   /**
-   * Converts `value` to an integer. (ES2016)
+   * Converts `value` to an integer. (ES2016).
    *
    * @param {*} value - The value to convert.
    * @returns {number} Returns the converted integer.
@@ -64,12 +66,12 @@ module.exports = {
    * toInteger(3); // 3
    * toInteger(Number.MIN_VALUE); // 0
    * toInteger(Infinity); // 1.7976931348623157e+308
-   * toInteger('3'); // 3
+   * toInteger('3'); // 3.
    */
   toInteger2016: $toInteger2016,
 
   /**
-   * Converts `value` to an integer. (ES2018)
+   * Converts `value` to an integer. (ES2018).
    *
    * @param {*} value - The value to convert.
    * @returns {number} Returns the converted integer.
@@ -79,7 +81,7 @@ module.exports = {
    * toInteger(3); // 3
    * toInteger(Number.MIN_VALUE); // 0
    * toInteger(Infinity); // 1.7976931348623157e+308
-   * toInteger('3'); // 3
+   * toInteger('3'); // 3.
    */
-  toInteger2018: $toInteger2018
+  toInteger2018: $toInteger2018,
 };
