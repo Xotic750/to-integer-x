e81dbd87d8887a0e13b96ad08706e56e2c5d62c6
Graham Fairweather <xotic750@gmail.com>
2019-08-07 23:57:37 +0200

    Lodash security fix

--- dist/to-integer-x.js
+++ dist/to-integer-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-2017",
-  "date": "2019-08-07T01:07:04.307Z",
+  "date": "2019-08-07T21:57:32.116Z",
   "describe": "",
   "description": "ToInteger converts 'argument' to an integral numeric value.",
   "file": "to-integer-x.js",
